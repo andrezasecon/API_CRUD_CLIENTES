22dc0ea8e05b87e6dada5332548acf8b4cb801c0
andrezasecon <andrezasecon@gmail.com>
2021-05-25 16:34:32 -0300

    Controller Service and exceptions

---
+++ src/main/java/com/andrezasecon/apiclientes/controllers/ClientController.java
@@ -0,0 +1,63 @@
+package com.andrezasecon.apiclientes.controllers;
+
+import com.andrezasecon.apiclientes.dto.ClientDto;
+import com.andrezasecon.apiclientes.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientController {
+
+    @Autowired
+    ClientService clientService;
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDto>> findAllClients(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction
+    ) {
+
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+
+        Page<ClientDto> list = clientService.findAllPageble(pageRequest);
+        return ResponseEntity.ok().body(list);
+
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDto> findCategoryById(@PathVariable Long id) {
+        ClientDto dto = clientService.findClientById(id);
+        return ResponseEntity.ok().body(dto);
+
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientDto> insertCategory(@RequestBody ClientDto dto) {
+        dto = clientService.insertClient(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClientDto> updateCategory(@PathVariable Long id, @RequestBody ClientDto dto) {
+        dto = clientService.updateClient(id, dto);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<ClientDto> deleteCategory(@PathVariable Long id) {
+        clientService.deleteClient(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/andrezasecon/apiclientes/controllers/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.andrezasecon.apiclientes.controllers.exceptions;
+
+import com.andrezasecon.apiclientes.services.exceptions.DataBaseException;
+import com.andrezasecon.apiclientes.services.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
+
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Resource not found");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<StandardError> dataBase(DataBaseException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("DataBase Exception");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
